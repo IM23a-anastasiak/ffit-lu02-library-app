260ee9452ade3ddcc766ed871f17518210eff749
IM23a-anastasiak <anastasiak@bzz.ch>
2025-08-26 10:42:15 +0200

    Task 1 & Task 2 completed

---
+++ src/main/java/ch/bzz/Book.java
@@ -0,0 +1,27 @@
+package ch.bzz;
+public class Book {
+    private int id;
+    private String isbn;
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(int id, String isbn, String title, String author, int year) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+
+    }
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -1,8 +1,41 @@
 package ch.bzz;
+import java.util.Scanner;
 
 public class LibraryAppMain {
 
+    private static final Book
+            BOOK_1 = new Book(1, "978-3-8362-9544-4", "Java ist auch eine Insel", "Christian Ullenboom", 2023),
+            BOOK_2 = new Book(2, "978-3-658-43573-8", "Grundkurs Java", "Dietmar Abts", 2024);
+
+
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        Scanner myObj = new Scanner(System.in);
+        String command = "";
+        boolean running = true;
+
+        while (running) {
+            System.out.println("Gebe einen Befehl an: ");
+            command = myObj.nextLine();
+
+            if (command.equals("quit")) {
+                System.out.println("Programm wird beendet.");
+                running = false;
+            } else if (command.equals("help")) {
+                System.out.println("Liste der Befehle:");
+                System.out.println("quit - Programm beenden");
+                System.out.println("help - Liste der Befehle anzeigen");
+            }
+            else if (command.equals("listbooks")){
+                System.out.println("Liste der Bücher:");
+                System.out.println(BOOK_1);
+                System.out.println(BOOK_2);
+            }else {
+                System.out.println("Unbekannter Befehl: " + command);
+
+            }
+        }
+        myObj.close();
+
     }
 }
